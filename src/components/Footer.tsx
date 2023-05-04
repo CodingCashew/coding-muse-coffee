153bad4be3a574aadf360bc6d1114c06613ade18
LizBlackledge <Liz5112@users.noreply.github.com>
2023-05-03 21:13:17 -0600

    center footer and style faqs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,79 @@
-
 import React from "react";
-import { FaFacebookSquare, FaInstagramSquare, FaTwitter, FaYoutube } from 'react-icons/fa';
-import { SiBuymeacoffee } from 'react-icons/si';
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import { SiBuymeacoffee } from "react-icons/si";
 import { Box, Text, Link, Container, Flex } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
-    <Box bg='gray.50' className="footer" minW="4xl">
-        <Flex className="flexboxContainer" maxW="4xl" gap={3} justify="center" p={3}>
-          <Link href="https://twitter.com/AmericanEnglishForDevs" className="footerLinks"><FaTwitter className="search" size={35}  color="primary.main" /></Link>
-          <Link href="https://www.facebook.com/profile.php?id=0" className="footerLinks"><FaFacebookSquare className="search" size={35}  color="primary.main" /></Link>
-          <Link href="https://www.instagram.com/AmericanEnglishForDevs/" className="footerLinks"><FaInstagramSquare className="search" size={35}  color="primary.main" /></Link>
-          <Link href="https://www.youtube.com/channel/0" className="footerLinks"><FaYoutube className="search" size={35}  color="primary.main" /></Link>
-          </Flex>
-          <Flex className="flexboxContainer" maxW="2xl" gap={3} justify="center" p={3}>
-          <Link href="/contact" className="footerLinks"><Text fontSize='lg' className="footerText">Contact</Text></Link>
-          <Link href="/newsletter" className="footerLinks"><Text fontSize='lg' className="footerText">Newsletter</Text></Link>
-          <Link href="/privacy-policy" className="footerLinks"><Text fontSize='lg' className="footerText">Privacy Policy</Text></Link>
-          <Link href="/terms-of-service" className="footerLinks"><Text fontSize='lg' className="footerText">Terms of Service</Text></Link>
+    <Box bg="gray.50" className="footer"  minW="8xl">
+      <Flex
+        className="flexboxContainer"
+        // maxW="8xl"
+        minW="7xl"
+        gap={5}
+        justify="center"
+        p={3}
+      >
+        <Link
+          href="https://twitter.com/AmericanEnglishForDevs"
+          className="footerLinks"
+        >
+          <FaTwitter className="search" size={35} color="primary.main" />
+        </Link>
+        <Link
+          href="https://www.facebook.com/profile.php?id=0"
+          className="footerLinks"
+        >
+          <FaFacebookSquare className="search" size={35} color="primary.main" />
+        </Link>
+        <Link
+          href="https://www.instagram.com/AmericanEnglishForDevs/"
+          className="footerLinks"
+        >
+          <FaInstagramSquare
+            className="search"
+            size={35}
+            color="primary.main"
+          />
+        </Link>
+        <Link href="https://www.youtube.com/channel/0" className="footerLinks">
+          <FaYoutube className="search" size={35} color="primary.main" />
+        </Link>
       </Flex>
+      <Flex
+        className="flexboxContainer"
+        // maxW="6xl"
+        minW="7xl"
+        gap={7}
+        justify="center"
+        p={3}
+      >
+        <Link href="/contact" className="footerLinks">
+          <Text fontSize="lg" className="footerText">
+            Contact
+          </Text>
+        </Link>
+        <Link href="/newsletter" className="footerLinks">
+          <Text fontSize="lg" className="footerText">
+            Newsletter
+          </Text>
+        </Link>
+        <Link href="/privacy-policy" className="footerLinks">
+          <Text fontSize="lg" className="footerText">
+            Privacy Policy
+          </Text>
+        </Link>
+        <Link href="/terms-of-service" className="footerLinks">
+          <Text fontSize="lg" className="footerText">
+            Terms of Service
+          </Text>
+        </Link>
+      </Flex> 
     </Box>
   );
 }
--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,14 +1,72 @@
 import React from "react";
 import { Container, Text } from "@chakra-ui/react";
-import Head from 'next/head'
+import Head from "next/head";
+
+interface faq {
+  question: string;
+  answer: string;
+}
+
+const questions: faq[] = [
+  {
+    question: "What if I don't like it?",
+    answer: "First, you can listen to the sample audios to see how well you like the speaking style. More importantly, if the content did not rock your world, you are eligible for a 110% money-back guarantee.",
+  },
+  {
+    question: "But will I really learn that much?",
+    answer: "Definitely! Not only will you learn a ton, but the language content is directly related to software engineering, so you can obtain the job of your dreams and excel when communicating with the rest of your team. ",
+  },
+  {
+    question: "Why is it so expensive?",
+    answer: "Not only is the target material specific to your needs, it would save you dozens of hours trying to compile separate materials from various sources. In addition, the audio is all recorded in a standard American accent, so you can be confident and clearly communicate with others.",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+  {
+    question: "",
+    answer: "",
+  },
+];
 
 export default function Faq() {
   return (
-    <Container minH="xl">
+    <Container minW="6xl" minH="xl">
       <Head>
         <title>FAQs</title>
       </Head>
-      <Text>Frequently Asked Questions</Text>
+      <Text fontSize="2xl" color="primary.dark">Frequently Asked Questions</Text>
+      {questions.map((faq: faq, index) => (
+        <Container key={index} minW="4xl" mt={5}>
+          <Text fontSize="2xl" color="secondary.light">{faq.question}</Text>
+          <Text fontSize="xl">{faq.answer}</Text>
+        </Container>
+      ))}
     </Container>
   );
 }
