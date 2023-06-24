dd9c30c902ac64093046470c5e8fdb28e722658f
lizblackledge <lizblackledge@users.noreply.github.com>
2023-06-24 08:18:40 -0500

    Render Paypal Buttons

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -13,24 +13,18 @@ import {
   ShoppingCartProvider,
 } from "../context/ShoppingCartContext";
 import Head from "next/head";
-import Script from "next/script";
-import { loadScript } from "@paypal/paypal-js";
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-import Paypal from "@/components/Paypal";
 
 export default function Checkout() {
   const { cartItems, subtotal } = useShoppingCart();
 
   const paypal = useRef();
 
+  const total = subtotal()
   useEffect(() => {
     if (window.paypal) {
 
       window.paypal.Buttons({
         createOrder: (data, actions, err) => {
-          // console.log('data: ', data)
-          // console.log('actions: ', actions)
-          // console.log('err: ', err)
           return actions.order.create({
             intent: "CAPTURE",
             purchase_units: [
@@ -38,7 +32,7 @@ export default function Checkout() {
                 description: "American English Course for Devs",
                 amount: {
                   currency_code: "USD",
-                  value: 650.00
+                  value: total
                 }
               }
             ]
@@ -46,7 +40,6 @@ export default function Checkout() {
         },
         onApprove: async (data, actions) => {
           const order = await (actions.order.capture())
-          // const order = await (actions.order.CAPTURE)
           console.log('Order:', order)
         },
         onError: (err) => {
@@ -56,30 +49,17 @@ export default function Checkout() {
     }
   }, [])
 
-
-  // const addPaypalScript = () => {
-  //   const script = document.createElement('script');
-  //   script.src = "https://www.paypal.com/sdk/js?client-id=AT2icWsG7fv_6swdjSGpEot6gPzCqyJk9LQM74lgbZm8snjmCK8hY01enIeqT4MVv2EQjal_UsDEFuiJ"
-  //   script.type = "text/javascript"
-  //   script.async = true;
-  //   document.body.appendChild(script)
-  // }
-
-  // useEffect(() => {
-  //   addPaypalScript()
-  // }, [])
-
-
   return (
     <Container minH="xl">
       <Head>
         <title>Checkout</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <script src="https://www.paypal.com/sdk/js?client-id=AT2icWsG7fv_6swdjSGpEot6gPzCqyJk9LQM74lgbZm8snjmCK8hY01enIeqT4MVv2EQjal_UsDEFuiJ" async></script>
+      <script src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`} async></script>
       <Stack mt={20}>
         <Text fontSize="2xl" mt={10}>Checkout</Text>
         <Divider mb={7} />
+        {/* {'subtotal: ' + subtotal} */}
         {cartItems &&
           cartItems.map((item, index) => (
             <Flex key={index} gap={5}>
@@ -93,9 +73,7 @@ export default function Checkout() {
           ))}
         <Divider mb={7} />
         <Text fontSize="2xl">Order Subtotal: ${subtotal()}</Text>
-              <div ref={paypal}></div>
-        {/* <Paypal /> */}
-        {/* <PayPalScriptProvider ref={paypal} options={{ "clientId": process.env.PAYPAL_CLIENT_ID }}/> */}
+        <Container ref={paypal} mt={5}></Container>
       </Stack>
     </Container>
   );
