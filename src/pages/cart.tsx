aaa84436693382f935d0f9813f16ee4af1fd8463
lizblackledge <lizblackledge@users.noreply.github.com>
2023-10-23 12:54:06 -0500

    Style cart, increment/decrement, discount code

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -10,6 +10,7 @@ import {
   Input,
   Divider,
   Box,
+  useToast,
 } from "@chakra-ui/react";
 import {
   useShoppingCart,
@@ -19,145 +20,252 @@ import Head from "next/head";
 import { ArrowRightIcon } from "@chakra-ui/icons";
 
 export default function Cart() {
-  const { cartItems, numOfItems, subtotal, removeItem } = useShoppingCart();
+  const { cartItems, numOfItems, subtotal, removeItem, increment, decrement } =
+    useShoppingCart();
   const [showDiscountInput, setShowDiscountInput] = useState(false);
+  const [discountCode, setDiscountCode] = useState<string>("");
+  const [percentToPay, setPercentToPay] = useState<number>(100);
+  const [invalidDiscountCode, setInvalidDiscountCode] = useState(false);
+
+  const toast = useToast();
 
   const toggleShowDiscountInput = () => {
     setShowDiscountInput(!showDiscountInput);
+    setInvalidDiscountCode(false);
   };
   const handleSubmitDiscountCode = () => {
-    console.log("Discount Code Submitted!");
+    const hardcodedDiscountCodes = {
+      eggplant778: "50",
+      snorkel322: "80",
+      bananas556: "90",
+    };
+    if (hardcodedDiscountCodes.hasOwnProperty(discountCode)) {
+      const percentToPay = Number(
+        hardcodedDiscountCodes[
+          discountCode as keyof typeof hardcodedDiscountCodes
+        ]
+      );
+      toast({
+        title: "Success",
+        description: `Discount Code added for ${100 - percentToPay}% off.`,
+        status: "success",
+        duration: 4000,
+        isClosable: true,
+      });
+
+      setPercentToPay(percentToPay);
+      setInvalidDiscountCode(false);
+      toggleShowDiscountInput();
+    } else {
+      setInvalidDiscountCode(true);
+    }
   };
 
-  const orderTotal = subtotal();
+  const handleUpdateDiscountCode = (e: any) => {
+    setDiscountCode(e.target.value);
+  };
+
+  const orderTotal =
+    percentToPay !== 100 ? (subtotal() * percentToPay) / 100 : subtotal();
+
   return (
     <Box bgColor="black" mt={20}>
-    <Container minH="2xl" minW={{ base: "xs", sm: "sm", md: "3xl", lg: "5xl" }} mt={20}>
-      <Head>
-        <title>Cart</title>
-        <link rel="icon" href="/coding-muse-icon.ico" />
-      </Head>
-      <Text fontSize="2xl" pt={10} color="primary.main">
-        Cart
-      </Text>
-      <Flex wrap={{ base: "wrap", lg: "nowrap" }} justify="center">
-        <Container minW="xl">
-          {!cartItems.length && (
-            <Container>
-              <Text mt={10} mb={5} color='secondary.light'>
-                Your cart is empty :/
-              </Text>
-              <Link href="/shop">
-                <Button color="white" bgColor="secondary.main" m={3}>
-                  Add a Coding Muse
-                  <ArrowRightIcon ml={3} />
-                </Button>
-              </Link>
-            </Container>
-          )}
-          {cartItems &&
-            cartItems.map((item) => (
-              <Container
-                key={Math.random() * 1000}
-                mt={5}
-                maxW={{ base: "xs", sm: "sm", md: "md", xl: "lg" }}
-              >
-                <Text fontSize="xl" mt={7} mb={4}>
-                  {item.name} Coffee
+      <Container
+        minH="2xl"
+        minW={{ base: "xs", sm: "sm", md: "3xl", lg: "5xl" }}
+        mt={20}
+      >
+        <Head>
+          <title>Cart</title>
+          <link rel="icon" href="/coding-muse-icon.ico" />
+        </Head>
+        <Text fontSize="2xl" pt={10} color="white">
+          Cart
+        </Text>
+        <Flex wrap={{ base: "wrap", lg: "nowrap" }} justify="center">
+          <Container minW="xl">
+            {!cartItems.length && (
+              <Container>
+                <Text mt={10} mb={5} color="secondary.light">
+                  Your cart is empty :/
                 </Text>
-                <Image
-                  src={item.imagePath}
-                  alt={`${item.name} coffee`}
-                  w="100%"
-                />
-                <Text fontSize="lg" mt={3}>
-                  Price:{" "}
-                  <Text as="span" color="secondary.dark">
-                    ${item.price}
-                  </Text>
-                </Text>
-
-                <Button
-                  color="primary.main"
-                  variant="ghost"
-                  m={3}
-                  width="28%"
-                  onClick={() => removeItem(item.id)}
-                >
-                  Remove Item
-                </Button>
-                <Divider mb={10} />
-              </Container>
-            ))}
-        </Container>
-        {cartItems.length > 0 && (
-          <Container
-            maxW={{ base: "xs", sm: "sm", md: "sm", lg: "md", xl: "lg" }}
-          >
-            <Text fontSize="xl" minW="xl" mb={5}>
-              Order Summary
-            </Text>
-            <Flex justify="space-between">
-              <Text>Number of Items</Text>
-              <Text>{numOfItems()}</Text>
-            </Flex>
-            <Button
-              m={3}
-              mb={0}
-              variant="ghost"
-              color="secondary.main"
-              onClick={toggleShowDiscountInput}
-            >
-              Add Discount Code
-            </Button>
-            {showDiscountInput && (
-              <Container m={3} mt={1}>
-                <Input></Input>
-                <Button
-                  bgColor="secondary.main"
-                  color="white"
-                  onClick={handleSubmitDiscountCode}
-                  mt={3}
-                >
-                  Submit
-                </Button>
-                <Button
-                  variant="ghost"
-                  ml={3}
-                  onClick={toggleShowDiscountInput}
-                  mt={3}
-                >
-                  Cancel
-                </Button>
+                <Link href="/shop">
+                  <Button color="white" bgColor="secondary.main" m={3}>
+                    Add a Coding Muse
+                    <ArrowRightIcon ml={3} />
+                  </Button>
+                </Link>
               </Container>
             )}
+            {cartItems &&
+              cartItems.map((item) => (
+                <Container
+                  key={Math.random() * 1000}
+                  mt={5}
+                  maxW={{ base: "xs", sm: "sm", md: "md", xl: "lg" }}
+                >
+                  <Text fontSize="xl" mt={7} mb={4} color="primary.main">
+                    {item.name} Coffee
+                  </Text>
+                  <Image
+                    src={item.imagePath}
+                    alt={`${item.name} coffee`}
+                    w="100%"
+                  />
+                  <Text fontSize="lg" mt={3} color="primary.main">
+                    Price:{" "}
+                    <Text as="span" color="secondary.main">
+                      ${item.price}
+                    </Text>
+                  </Text>
+                  <Text fontSize="lg" mt={3} color="primary.main">
+                    Quantity:{" "}
+                    <Text as="span" color="secondary.main">
+                      {item.quantity}
+                    </Text>
+                  </Text>
 
-            <Flex justify="space-between">
-              <Text>Tax</Text>
-              <Text>TBD</Text>
-            </Flex>
-            <Flex justify="space-between">
-              <Text>Order Subtotal</Text>
-              <Text>${orderTotal}</Text>
-            </Flex>
-            <Container mb={10}>
-              <Link href="/checkout">
-                {!!cartItems.length && (
-                  <Button color="white" bgColor="primary.dark" m={3}>
-                    Checkout
+                  <Button
+                    color="white"
+                    variant="ghost"
+                    m={3}
+                    onClick={() =>
+                      increment(
+                        item.id,
+                        item.name,
+                        item.description,
+                        item.roast,
+                        item.region,
+                        item.price,
+                        item.size,
+                        item.imagePath
+                      )
+                    }
+                  >
+                    +
                   </Button>
-                )}
-              </Link>
-              <Link href="/shop">
-                <Button color="primary.dark" variant="outline" m={3}>
-                  Back to Coffees
-                </Button>
-              </Link>
-            </Container>
+                  <Button
+                    color="white"
+                    variant="ghost"
+                    m={3}
+                    onClick={() =>
+                      decrement(
+                        item.id,
+                        item.name,
+                        item.description,
+                        item.roast,
+                        item.region,
+                        item.price,
+                        item.size,
+                        item.imagePath
+                      )
+                    }
+                  >
+                    -
+                  </Button>
+                  <Button
+                    color="white"
+                    variant="ghost"
+                    m={3}
+                    width="28%"
+                    onClick={() => removeItem(item.id)}
+                  >
+                    Remove Item
+                  </Button>
+                  <Divider mb={10} />
+                </Container>
+              ))}
           </Container>
-        )}
-      </Flex>
-    </Container>
+          {cartItems.length > 0 && (
+            <Container
+              maxW={{ base: "xs", sm: "sm", md: "sm", lg: "md", xl: "lg" }}
+            >
+              <Text fontSize="xl" minW="xl" mb={5} color="white">
+                Order Summary
+              </Text>
+              <Flex justify="space-between">
+                <Text color="white">Number of Items</Text>
+                <Text color="secondary.main">{numOfItems()}</Text>
+              </Flex>
+              {percentToPay === 100 && (
+                <Button
+                  m={3}
+                  mb={0}
+                  variant="ghost"
+                  color="primary.main"
+                  onClick={toggleShowDiscountInput}
+                >
+                  Add Discount Code
+                </Button>
+              )}
+
+              {showDiscountInput && (
+                <Container m={3} mt={7} mb={7}>
+                  <Input
+                    bgColor="white"
+                    value={discountCode}
+                    onChange={handleUpdateDiscountCode}
+                  ></Input>
+                  {invalidDiscountCode && (
+                    <div>
+                      <Text color="primary.light">Invalid Discount Code</Text>
+                    </div>
+                  )}
+                  <Button
+                    bgColor="secondary.main"
+                    color="white"
+                    onClick={handleSubmitDiscountCode}
+                    mt={3}
+                  >
+                    Submit Code
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    ml={3}
+                    color="white"
+                    onClick={toggleShowDiscountInput}
+                    mt={3}
+                  >
+                    Cancel
+                  </Button>
+                </Container>
+              )}
+
+              <Flex justify="space-between">
+                <Text color="white">Tax</Text>
+                <Text color="secondary.main">TBD</Text>
+              </Flex>
+              <Flex justify="space-between" mt={5}>
+                <Text color="white" alignSelf="end">Order Subtotal</Text>
+
+                {percentToPay === 100 && (
+                  <Text color="secondary.main">${orderTotal}</Text>
+                )}
+                {percentToPay !== 100 && (
+                  <Flex direction="column">
+                    <Text id="oldPrice" color="white">${subtotal()}</Text>
+                    <Text color="secondary.main">${orderTotal}</Text>
+                  </Flex>
+                )}
+              </Flex>
+              <Container mb={10} mt={5}>
+                <Link href="/checkout">
+                  {!!cartItems.length && (
+                    <Button color="white" bgColor="primary.dark" m={3}>
+                      Checkout
+                    </Button>
+                  )}
+                </Link>
+                <Link href="/shop">
+                  <Button color="primary.dark" variant="ghost" m={3}>
+                    Back to Coffees
+                  </Button>
+                </Link>
+              </Container>
+            </Container>
+          )}
+        </Flex>
+      </Container>
     </Box>
   );
 }
